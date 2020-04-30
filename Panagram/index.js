8a61f9de1a057f4c8c5ec67ff69f401c63d38e23
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-04-30 21:53:05 +0200

    Add done task

---
+++ Panagram/index.js
@@ -0,0 +1,53 @@
+/*
+ * Opanuj JavaScript - Przeprogramowani.pl
+ * I. Fundamenty języka JavaScript
+ *
+ * Ćwiczenie 5 - "Pangram"
+ */
+
+/*
+ * Cel zadania
+ *------------
+ * Zaimplementuj funkcję, która sprawdzi, czy podany parametr to tzw. pangram.
+ *
+ * Pangram to możliwe do zrozumienia zdanie wykorzystujące wszystkie litery danego alfabetu.
+ *
+ *
+ * Przykład:
+ *
+ * isPangram('test') // => false
+ * isPangram('Dość gróźb fuzją, klnę, pych i małżeństw!') // => true
+ */
+
+/*
+ * Punkty dodatkowe
+ *-----------------
+ * Zweryfikuj, czy konkretna litera występuje w podanym zdaniu tylko jeden raz.
+ */
+
+
+/* Weryfikacja */
+
+function verify(input, goal) {
+    if (input === goal) {
+        console.log('Gratulacje!');
+    } else {
+        console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+    }
+}
+
+const alfabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż'];
+
+function isPangram(sentence) {
+
+    for(letter of alfabet){
+        if (sentence.toLowerCase().includes(letter) === false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+verify(isPangram('test'), false);
+verify(isPangram('Dość gróźb fuzją, klnę, pych i małżeństw!'), true);
+verify(isPangram('Dość gróźb fuzją, klnę, pych i małże!'), false);
